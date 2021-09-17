2e27998fd12e56958b3e068ba0761893a5642e5b
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2021-09-17 19:33:08 +0300

    Add support to scrape items from vehicles page

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,10 +1,17 @@
 import json
 import requests
 
-QL_URL = 'https://www.qatarliving.com/backend/api/classified/search'
+QL_URLS = {
+  'classifieds': 'https://www.qatarliving.com/backend/api/classified/search',
+  'vehicles': 'https://www.qatarliving.com/backend/api/vehicles'
+}
 
-def scrape(search_term, state_file):
-  current_items = _get_current_items(search_term)
+def scrape(search_term, state_file, vehicles):
+  if vehicles:
+    current_items = _get_current_items_from_vehicles(search_term)
+  else:
+    current_items = _get_current_items_from_classifieds(search_term)
+
   seen_items = _get_seen_items(state_file)
   new_items = _resolve_new_items(current_items, seen_items)
   # Merge current_items with seen_items.
@@ -23,16 +30,34 @@ def _resolve_new_items(current_items, seen_items):
 
   return new_items
 
-def _get_current_items(search_term):
-  response = requests.post(QL_URL, data={'title': search_term})
+def _get_current_items_from_classifieds(search_term):
+  response = requests.post(QL_URLS['classifieds'], data={'title': search_term})
   items = response.json()['classified']
   current_items = {}
 
   # Massage the response a bit, we don't need everything it returns.
   for item in items:
     _id = item['_id']
+
     current_items[_id] = {
       'title': item['_source']['title'],
+      'price': item['_source']['price']
+    }
+
+  return current_items
+
+def _get_current_items_from_vehicles(search_term):
+  response = requests.post(QL_URLS['vehicles'], data={'title': search_term})
+  items = response.json()['vehicle']
+  current_items = {}
+
+  for item in items:
+    _id = item['_id']
+
+    # Massage the response a bit, we don't need everything it returns.
+    current_items[_id] = {
+      'title': item['_source']['title'],
+      'vehicle': item['_source']['vehicle_title'],
       'price': item['_source']['price']
     }
 
